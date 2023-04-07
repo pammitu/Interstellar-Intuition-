fa8031d6db2a2d21286928c5946b86ecd3d60405
pammitu <pamstankova@gmail.com>
2023-04-07 05:27:02 -0700

    figuring out timer bug

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const startBtn = document.getElementById("start-button");
 const stats = document.getElementById("score");
 const spaceObjects = document.querySelectorAll(".space-object");
 
+
 let firstCard = null;
 let secondCard = null;
 let pairsFound = 0;
@@ -9,22 +10,60 @@ let badGuesses = 0;
 let score = 0;
 
 startBtn.addEventListener("click",startGame);
+let timerInterval; 
+
+function startTimer(){
+    let timeRemaining = 120;
+
+    timerInterval = setInterval(() => {
+        timeRemaining--;
+
+    const timerElement = document.getElementById("timer");
+    timerElement.textContent = "Time Remaining: " + timeRemaining + "s";
+
+if (timeRemaining <= 0) {
+    clearInterval(timerInterval);
+    timeUp();
+     }
+    }, 1000);
+}
+
+function timeUp() {
+    clearInterval(timerInterval);
+    showMessage("You ran out of time! Your final score is: " + score);
+   resetGame();
+}
+
+function resetGame(){
+    clearInterval(timerInterval);
+
+spaceObjects.forEach(spaceObject => {
+    hideCard(spaceObject);
+});
+
+setTimeout(() => {
+    startGame();
+    pairsFound = 0;
+    badGuesses = 0;
+    score = 0;
+    updateStats();
+}, 5000);
+}
 
 function startGame() {
-    //shuffle cards????????????????? how
-    
+ clearInterval(timerInterval);
+ startTimer();
     const spaceObjectsArray = Array.from(spaceObjects);
     spaceObjectsArray.sort(() => Math.random() - 0.5);
 
-    for(let i = 0; i<spaceObjectsArray.length; i++) {
+    for(let i = 0; i < spaceObjectsArray.length; i++) {
         spaceObjectsArray[i].style.order = i;
     }
-
-    //add click event listeners to cards???? 
 
     spaceObjects.forEach(spaceObject => {
         spaceObject.addEventListener("click", flipCard);
     });
+    hideMessage();
 }
 let isFlipping = false; 
 
@@ -45,10 +84,12 @@ function flipCard(event) {
         if (isMatch(firstCard, secondCard)) {
             pairsFound++;
             score += 100;
+            disableCards(firstCard, secondCard);
             firstCard = null;
             secondCard = null;
 
             if (pairsFound === 10) {
+                clearInterval(timerInterval);
                 showMessage('YOU WON!! FINAL SCORE IS: ' + score);
                 resetGame();
             }
@@ -58,8 +99,10 @@ function flipCard(event) {
             showMessage("Score: " + score + " You've made " + badGuesses + " bad guesses so far");
 
             if (badGuesses === 20) {
+                clearInterval(timerInterval)
                 showMessage("YOU LOST! FINAL SCORE IS: " + score);
-                resetGame();
+                restartGame();
+
             } else {
                     hideCard(firstCard);
                     hideCard(secondCard);
@@ -67,11 +110,6 @@ function flipCard(event) {
                     secondCard = null;
                 }
             }
-
-          if (pairsFound + badGuesses === 19) {
-                showMessage("You lost");
-                resetGame();
-        }
           
             updateStats();
             isFlipping = false;
@@ -83,16 +121,16 @@ function showCard(card) {
     const openFace = card.querySelector('.open-face');
     const hiddenSide = card.querySelector(".hidden-side");
 
-    openFace.style.opacity = 0;
-    hiddenSide.style.opacity = 1;
+    openFace.style.display = 'none';
+    hiddenSide.style.display = 'block';
 }
 
 function hideCard(card) {
     const openFace = card.querySelector('.open-face');
     const hiddenSide = card.querySelector('.hidden-side');
 
-    openFace.style.opacity = 1;
-    hiddenSide.style.opacity = 0;
+    openFace.style.display = 'block';
+    hiddenSide.style.display = 'none';
 }
 
 function isMatch(card1,card2) {
@@ -101,36 +139,52 @@ function isMatch(card1,card2) {
 
     return img1.src === img2.src;
 }
+function updateStats(){
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent = "Score: " + score;
 
-function updateStats() {
-    const pairsElement = document.getElementById('pairs');
-    const foulsElement = document.getElementById('fouls');
+    const pairsElement = document.getElementById("pairs");
+    pairsElement.textContent = "Pairs Found: " + pairsFound;
 
-    stats.textContent = "Score: " + score;
-    pairsElement.textContent = "Total Pairs: " + pairsFound;
-    foulsElement.textContent = "Fouls: " + badGuesses;
+    const foulsElement = document.getElementById("fouls");
+    foulsElement.textContent = "Bad Guesses: " + badGuesses;
 }
 
-function resetGame() {
-    spaceObjects.forEach(spaceObject => {
-        hideCard(spaceObject);
-    });
-
-    setTimeout(() => {
-        startGame();
-        pairsFound = 0;
-        badGuesses = 0;
-        score = 0;
-        updateStats();
-    }, 5000);
+function showMessage(text) {
+    const message = document.getElementById("message");
+    message.textContent = text;
+    message.style.display = 'block';
 }
 
-function showMessage(msg) {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = msg;
-    messageElement.style.display = 'block';
-
-    setTimeout(() => {
-        messageElement.style.display = 'none';
-    }, 3000);
+function hideMessage() {
+    const message = document.getElementById("message");
+    message.style.display = 'none';
 }
+
+function disableCards(card1, card2) {
+    card1.removeEventListener("click", flipCard);
+    card2.removeEventListener("click",flipCard);
+}
+
+const restartBtn = document.getElementById("restart-button");
+restartBtn.addEventListener("click",restartGame);
+
+function restartGame() {
+restartBtn.style.display = 'none';
+resetGame();
+}
+
+const stopBtn = document.getElementById("stop-button");
+stopBtn.addEventListener('click',pauseGame);
+
+let isPaused = false;
+function pauseGame() {
+    if(!isPause) {
+        clearInterval(timerInterval);
+        isPaused = true;
+    } else {
+        startTimer();
+        isPaused = false;
+    }
+}
+
